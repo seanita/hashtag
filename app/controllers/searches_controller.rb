e2b37d54e7340a8f3a66569b97b8da825d4d0fa7
Seanita Tolliver <seanitat@gmail.com>
2013-09-19 12:21:45 -0400

    Move Twitter logic to Searcher

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,10 @@
 class SearchesController < ApplicationController
-	
+
 	def create
 		redirect_to search_path(params[:twitter][:search])
 	end
 
 	def show
-		@tweets = Twitter.search("##{params[:id]}").results
+		@tweets = Searcher.new("##{params[:id]}").results
 	end
 end
